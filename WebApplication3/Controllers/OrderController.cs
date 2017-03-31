0bf9330021ea3045fcaf9275c299faa99a353750
1103137235 <1103137235@gm.kuas.edu.tw>
2017-03-31 15:22:09 +0800

    ajax

---
+++ WebApplication3/Controllers/OrderController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication3.Controllers
+{
+    public class OrderController : Controller
+    {
+        //
+        // GET: /Order/
+        public ActionResult Index()
+        {
+            List<SelectListItem> custData = new List<SelectListItem>();
+            custData.Add(new SelectListItem()
+            {
+                Text="瑞陽資訊",
+                Value="1"
+            });
+            custData.Add(new SelectListItem()
+            {
+                Text="往軟資訊",
+                Value="2"
+            });
+            ViewBag.custData = custData;
+            return View();
+        }
+        public ActionResult InsertOrder()
+        {
+            return View();
+        }
+        [HttpPost()]
+        public ActionResult InsertOrder(Models.Order order)
+        {
+            Models.OrderService orderService = new Models.OrderService();
+            orderService.InsertOrder(order);
+            return View("Index");
+        }
+        [HttpGet()]
+        public JsonResult TestJson()
+        {
+            var result = new Models.Order() { CustId = "GSS", CustName = "叡揚資訊" };
+            return this.Json(result, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ WebApplication3/Models/OrderService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication3.Models
+{
+    public class OrderService
+    {
+        public void InsertOrder(Models.Order order)
+        {
+        }
+        public Models.Order GetOrderById(string orderId)
+        {
+            Models.Order result = new Order();
+            result.CustId = "GSS";
+            result.CustName = "叡揚資訊";
+            return result;
+        }
+        public void DeleteOrderById(string orderId)
+        {
+        }
+        public void UpdateOrder(Models.Order order)
+        {
+        }
+        public List<Models.Order> GetOrders()
+        {
+            List<Models.Order> result = new List<Order>();
+            result.Add(new Order() { CustId = "GSS", CustName = "叡揚資訊", EmpId = 1, EmpName = "王小明", Orderdate = DateTime.Parse("2015/11/08") });
+            result.Add(new Order() { CustId = "NPOIS", CustName = "網軟資訊", EmpId = 2, EmpName = "李小華", Orderdate = DateTime.Parse("2015/11/01") });
+            return result;
+        }
+    }
+}
